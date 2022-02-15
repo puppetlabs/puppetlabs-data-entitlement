762aa4cb773862262bec5c2abe94018957b30a32
Andrew C Wrenn <awrenn53@gmail.com>
2022-02-15 12:13:22 -0800

    Rename the HDP::Url type

--- types/url.pp
+++ types/url.pp
@@ -1,4 +1,4 @@
-# HDP::Url is a metatype that supports both single and multiple urls
+# DataEntitlement::Url is a metatype that supports both single and multiple urls
 #
 # @summary DataEntitlement::Url is a metatype that supports both single and multiple urls
 type DataEntitlement::Url = Variant[Array[Stdlib::HTTPUrl], Stdlib::HTTPUrl]
